60b4b866dd4d63b4f9317f5537114d58fa5cc88f
Amy Liao <amzhliao@gmail.com>
2024-12-08 19:24:50 -0600

    day3 part1 done

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,19 +1,69 @@
 #include <string>
 #include <vector>
+#include <sstream>
 #include "../utils.h"
 
 using namespace std;
 
+int validMults(string s)
+{
+  int result = 0;
+
+  size_t start = 0;
+  size_t index = s.find("mul(", start);
+  while (index != string::npos)
+  {
+    string rest = s.substr(index + 4);
+    stringstream ss(rest);
+
+    // what we want to read from stream
+    int x;
+    char comma;
+    int y;
+    char close;
+
+    // try to read from stream
+    if (true                           //
+        && ss >> x && x <= 999         //
+        && ss >> comma && comma == ',' //
+        && ss >> y && y <= 999         //
+        && ss >> close && close == ')' //
+    )
+    {
+      result += x * y;
+    }
+
+    // prep for next loop
+    start = index + 1;
+    index = s.find("mul(", start);
+  }
+
+  return result;
+}
+
 int main()
 {
-  string test = "hi my name is amy";
-  string delimiter = " ";
-  vector<string> tokens = split(test, delimiter);
+  ifstream inputFile("input.txt");
 
-  // print out tokens
-  for (int i = 0; i < tokens.size(); i++)
+  if (!inputFile.is_open())
   {
-    cout << tokens[i] << ",";
+    cerr << "Could not open the file!" << endl;
+    return 1;
   }
-  cout << endl;
+
+  /**
+   * Part 1
+   */
+
+  int result = 0;
+  string line;
+  while (getline(inputFile, line))
+  {
+    result += validMults(line);
+  }
+  inputFile.close();
+
+  cout << "Part 1 Result: " << result << endl;
+
+  return 0;
 }
